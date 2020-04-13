d5f0d2c244133a494966ec4657db6facbefc0c6c
Görkem Güray <grkmgry@gmail.com>
2020-04-13 12:25:32 +0300

    ders 168 kullanıcı login

--- blog.py
+++ blog.py
@@ -14,10 +14,16 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField("Parola Doğrula")
 
+
+class LoginForm(Form):
+    username = StringField("Kullanıcı adı")
+    password = PasswordField("Parola")
+
  
 
 
 app = Flask(__name__)
+app.secret_key="gorkem.co"
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
@@ -26,6 +32,7 @@ app.config["MYSQL_DB"] = "grkm_blg"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
+
 
 
 
@@ -58,17 +65,52 @@ def register():
 
         cursor = mysql.connection.cursor()
 
-        sorgu = "Insert into users(name,email,username,password) VALUES(%s,%s,%s,%s)"
+        sorgu = "Insert into users(name,username,email,password) VALUES(%s,%s,%s,%s)"
 
         cursor.execute(sorgu,(name,username,email,password,))
         mysql.connection.commit()
         cursor.close()
 
 
-
-        return redirect(url_for("index"))
+        flash(message="Başarıyla kayıt oldunuz...", category="success")
+        return redirect(url_for("login"))
     else:
         return render_template("register.html", form=form)
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+
+    form = LoginForm(request.form)
+    if request.method =="POST":
+        username = form.username.data
+        password_entered = form.password.data
+
+        cursor = mysql.connection.cursor()
+
+        sorgu = "Select * from users where username = %s"
+
+        result = cursor.execute(sorgu, (username,))
+
+        if result > 0:
+            data = cursor.fetchone()
+            real_password = data["password"]
+
+            if sha256_crypt.verify(password_entered, real_password):
+                flash(message="Başarıyla giriş yaptınız...", category="success")
+                return redirect(url_for("index"))
+            else:
+                flash(message="Parola yanlış...", category="danger")
+                return redirect(url_for("login"))
+
+
+
+
+        else:
+            flash(message="Kullanıcı bulunamadı", category="danger")
+            return redirect(url_for("login"))
+
+    return render_template("login.html",form=form)
+
     
 
 
